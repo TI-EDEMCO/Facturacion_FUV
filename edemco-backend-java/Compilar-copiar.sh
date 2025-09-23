56864145853ef6719d37ca98b160512ba36229a2
JoseEdemco <jose.romero@edemco.co>
2025-09-23 09:12:23 -0500

    Cambios sobre compilados. Se crea archivo para complilar los microservicios y copiarlos en la carpeta de compilados para facilitar el proceso. se eliminan archoivos .jar para eviatr conflictos de merge.

---
+++ edemco-backend-java/Compilar-copiar.sh
@@ -0,0 +1,28 @@
+
+
+pathCompilados="C:\edemco\compilados-edemco"
+
+for dir in */ ; do
+
+    #Endicar que microservicio esta compilando
+    echo "================================="
+    echo "compilando microservicio : $dir"
+    echo "================================="
+
+    #Entrar a la ruta del microservicio
+    cd "$dir" || exit
+
+    #Ejecitar el comando para compilar
+    mvn clean package -DskipTests 
+
+    #Copiar todos los archivos .jar en la capeta designada
+    cp target/*.jar "$pathCompilados"
+
+    #Volver a la raiz
+    cd ..
+done
+echo "================================="
+echo "SE TERMINO DE COMPILAR LOS MICROSERVICIOS"
+echo "================================="
+
+sleep 10s
