be7d30ee80c7361a2efc73229317dbb6590f65ce
unknown <usuario@AUTOMATIZACIONES.grupoedemco.local>
2025-11-26 14:35:21 -0500

    Se aumenta el tiempo de espera para tomar la informacion de growat

--- edemco-frontend-react/src/components/pages/IniciarFactura/IniciarFactura.jsx
+++ edemco-frontend-react/src/components/pages/IniciarFactura/IniciarFactura.jsx
@@ -124,7 +124,7 @@ const IniciarFactura = () => {
         console.error("Failed to get data from growatt:", result.error);
         setIsLoading(false);
       }
-    }, 50000);
+    }, 70000);
   };
 
   const uploadFile = async (excelFile) => {
